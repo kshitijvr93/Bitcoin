12aa267170510c8200d2300be744f388936f5efe
anuragit123 <37980426+anuragit123@users.noreply.github.com>
2018-11-26 04:37:17 -0500

    completed part 1 XD

--- lib/DS.ex
+++ lib/DS.ex
@@ -4,8 +4,13 @@ defmodule DS do
         IO.puts(b)
         highestNodePossible=100
         {_,spid}=DSS.start_link([])
+
         DSS.start_child(spid,0,highestNodePossible)
+
         genServerGenerator(spid,1,highestNodePossible)
+
+        # all children started!!
+
     end
 
     def genServerGenerator(spid,currentNode,highestNodePossible) do
--- lib/Nodes.ex
+++ lib/Nodes.ex
@@ -10,10 +10,168 @@ defmodule Nodes do
         keys = [public_key,private_key]
         data2= [selfNum,highestNodePossible,keys]
         id="id#{selfNum}"
+        state = 
+        if selfNum==0 do
+            data2
+        else
+            wallet_value = 5
+            current_transaction_pool_list = []
+            ledger = []
+            transactionsNewBlockWork = []
+            [selfNum,highestNodePossible,keys,wallet_value, transactionsNewBlockWork,current_transaction_pool_list, ledger]
+        end
         
-        
-        GenServer.start_link(__MODULE__,data2,name: :"#{id}")# for zero the maxNodes
+        GenServer.start_link(__MODULE__,state,name: :"#{id}")# for zero the maxNodes
     end
+
+    def handle_cast({:receivetransactionList, transacList},state) do #for !0
+        IO.puts("receivetransactionList")
+        IO.inspect(state)
+        [selfNum,highestNodePossible,keys,wallet_value, transactionsNewBlockWork ,current_transaction_pool_list, ledger] = state
+        current_transaction_pool_list= current_transaction_pool_list++ transacList
+         state =  [selfNum,highestNodePossible,keys,wallet_value, transactionsNewBlockWork,current_transaction_pool_list, ledger]
+         
+        
+       
+        {:noreply,state}
+    end
+
+     def handle_cast(:generateBlock,state) do #for 0
+        # IO.puts("generateBlock")
+        [selfNum,highestNodePossible,keys,wallet_value, transactionsNewBlockWork, current_transaction_pool_list, ledger] = state
+
+         #state =  [selfNum,highestNodePossible,keys,wallet_value, transactionsNewBlockWork, transacList, ledger]
+         state=
+         if length(transactionsNewBlockWork) == 0 do
+            if length(current_transaction_pool_list) == 0 do
+                # IO.puts("1")
+                GenServer.cast(:"id#{selfNum}",:generateBlock)
+                state
+            else
+                # IO.puts("1")
+                GenServer.cast(:"id#{selfNum}",{:mineCoin,current_transaction_pool_list,:rand.uniform(10000000),System.system_time(:millisecond)})
+                state =  [selfNum,highestNodePossible,keys,wallet_value, current_transaction_pool_list, [], ledger]
+                
+                state
+            end
+         else
+            # IO.puts("1")
+            # GenServer.cast(:"id#{selfNum}",{:mineCoin,current_transaction_pool_list,:rand.uniform(10000000),System.system_time(:millisecond)})
+            # state =  [selfNum,highestNodePossible,keys,wallet_value, current_transaction_pool_list, [], ledger]
+            
+            state
+         end
+        
+        {:noreply,state}
+        
+    end
+    
+    def handle_call(:generateBlockStart,_from,state) do
+        Enum.each(1..100,fn(x)->
+            GenServer.cast(:"id#{x}",:generateBlock)
+        end)
+
+        {:reply,state,state}
+    end
+
+    def handle_cast({:mineCoin,data,currentNonce,startTime},state) do
+        # IO.puts("mineCoin")
+        [selfNum,highestNodePossible,keys,wallet_value, transactionsNewBlockWork, current_transaction_pool_list, ledger] = state
+        
+        state =
+        if transactionsNewBlockWork == data do
+            
+            concatData= concatTransactions(data,"")
+
+            hashValue = Base.encode16(:crypto.hash(:sha256,concatData<>to_string(currentNonce)))
+            # =Base.encode32(:crypto.hash(:sha256,"hello"<>to_string(108)))
+           
+            if String.slice(hashValue,0,4) == "0000" do
+                IO.puts(hashValue)
+                IO.puts(currentNonce)
+                IO.puts(System.system_time(:millisecond)-startTime)
+
+                blockData = concatData<>"|| the current NONCE is #{currentNonce} "
+                blockHeader = ""
+                block = {blockHeader,blockData}
+                ledger = ledger ++ [block]
+                GenServer.cast(:"id#{selfNum}",{:mineCoin,data,currentNonce+1,startTime})
+                [selfNum,highestNodePossible,keys,wallet_value, [], current_transaction_pool_list, ledger]
+                ##### to fill
+            else           
+                GenServer.cast(:"id#{selfNum}",{:mineCoin,data,currentNonce+1,startTime})
+                state
+            end
+        
+        else
+            GenServer.cast(:"id#{selfNum}",:generateBlock)
+            state
+
+        end
+        
+        
+        {:noreply,state}
+
+    end
+    
+    def concatTransactions(transactionsNewBlockWork, mergedTransactions) do
+        # IO.puts("concatTransactions")
+        if length(transactionsNewBlockWork) >0 do
+            current = Enum.at(transactionsNewBlockWork,0)
+            mergedTransactions= mergedTransactions<>"||"<>current
+            concatTransactions(transactionsNewBlockWork--[current],mergedTransactions)
+        else
+            mergedTransactions
+        end
+    end
+    
+    def handle_call(:getState,_from,state) do
+        IO.inspect(state)
+
+        {:reply,state,state}
+    end
+
+    def handle_cast({:createDummyValues,counter,w8time,transactionNumber},state) do #for 0
+         IO.puts("createDummyValues!!")
+        if counter > 0 do
+            if System.system_time(:millisecond) - w8time >10000 do
+                transactionList = generateRandomTransactions(:rand.uniform(5),[])
+                IO.inspect(transactionList)
+                spreadTransactions(1,100,transactionList)
+
+                GenServer.cast(:id0,{:createDummyValues,counter-1,System.system_time(:millisecond),transactionNumber+length(transactionList)})
+            else
+                GenServer.cast(:id0,{:createDummyValues,counter,w8time,transactionNumber})
+            end
+       
+        end
+
+        {:noreply,state}
+    end
+    
+    def spreadTransactions(current, max, transactionList) do
+        IO.puts("spreadTransactions running")
+        if current<=max do
+            GenServer.cast(:"id#{current}",{:receivetransactionList,transactionList})
+            spreadTransactions(current+1, max, transactionList)
+        end
+
+    end
+
+
+    def generateRandomTransactions(transNum, transactionList) do
+        if transNum == 0 do
+            transactionList
+        else
+            senderNode = :rand.uniform(99)+1
+            receiverNode = :rand.uniform(99)+1
+            money = :rand.uniform(1000)
+            dummyString = "#{senderNode} #{money} #{receiverNode}"
+            transactionList = transactionList ++ [dummyString]
+            generateRandomTransactions(transNum-1, transactionList)
+        end
+    end
+
     
     # def genPrivateKey() do
 
@@ -56,19 +214,6 @@ defmodule Nodes do
     end
 
 
-    def handle_cast({:mineCoin,data,numberOfZero,currentNonce,startTime},state) do
-       
-        hashValue = Base.encode16(:crypto.hash(:sha256,data<>to_string(currentNonce)))
-        # =Base.encode32(:crypto.hash(:sha256,"hello"<>to_string(108)))
-        [selfNum,_,_]=state
-        if String.slice(hashValue,0,4) == "0000" do
-            IO.puts(hashValue)
-            IO.puts(currentNonce)
-            IO.puts(System.system_time(:millisecond)-startTime)
-        else
-            GenServer.cast(:"id#{selfNum}",{:mineCoin,data,numberOfZero,currentNonce+1,startTime})
-        end
-        {:noreply,state}
-    end
+    
 
 end
